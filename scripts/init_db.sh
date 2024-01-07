234c05809ab03fddaac8fec29c26163d46b2cace
Setsubou <Setsubou@protonmail.com>
2024-01-07 13:55:45 +0700

    Fix typos on the scripts

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,16 +2,21 @@
 set -x
 set -eo pipefail
 
-if ! [-x "$(command -v psql)"]; then
-    echo >&2 "Error: psql is not installed"
-    exit 1
-fi
-
-if ! [-x "$(command -v sqlx)"]; then
+if ! [ -x "$(command -v sqlx)" ]; then
     echo >&2 "Error: sqlx is not installed"
     echo >&2 "Use:"
     echo >&2 "  cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
     echo >&2 "to install it."
+    exit 1
+fi
+
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "Error: psql is not installed"
+    exit 1
+fi
+
+if ! [ -x "$(command -v docker)" ]; then
+    echo >&2 "Error: docker is not installed"
     exit 1
 fi
 
@@ -25,7 +30,8 @@ DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 
 # Allow to skip docker if Postgres database is already running on docker
-if [[-z "${SKIP_DOCKER}" ]]; then
+if [[ -z "${SKIP_DOCKER}" ]]; then
+    echo >&2 "Creating a new Postgres Container"
     #Launch Postgres using Docker
     docker run \
         -e POSTGRES_USER=${DB_USER} \
@@ -35,6 +41,7 @@ if [[-z "${SKIP_DOCKER}" ]]; then
         -d postgres \
         postgres -N 1000
 fi
+echo >&2 "Docker Launched, will try to ping Postgres now"
 
 #Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
@@ -42,7 +49,7 @@ until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q';
     echo >&2 "Postgres is still unavailable - sleeping"
     sleep 1
 done
-echo >&2 "Postgres is up and running on port ${DB_PORT} - Running migrations now!"
+echo >&2 -e "\e[32mPostgres is up and running on port ${DB_PORT} - Running migrations now!\e[0m"
 
 export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}"
 sqlx database create
