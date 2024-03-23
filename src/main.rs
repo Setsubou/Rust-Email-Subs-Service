cefa227e257a3fbc41abd3ae80b978f3f8f295ac
Setsubou <Setsubou@protonmail.com>
2024-03-24 00:33:57 +0700

    Clippy

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::net::TcpListener;
 use secrecy::ExposeSecret;
+use std::net::TcpListener;
 
 use sqlx::PgPool;
-use zero2prod::{configuration::get_configuration, startup::run, telemetry::{get_subscriber, init_subscriber}};
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -10,9 +14,14 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration");
 
-    let connection = PgPool::connect(&configuration.database.get_connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection = PgPool::connect(
+        configuration
+            .database
+            .get_connection_string()
+            .expose_secret(),
+    )
+    .await
+    .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
