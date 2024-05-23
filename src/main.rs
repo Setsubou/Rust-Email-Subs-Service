447e14224c878e8aa6a267b507849421cef2368e
Setsubou <setsubou@protonmail.com>
2024-05-23 12:33:42 +0700

    Changed code to get env var based off the new configuration

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use secrecy::ExposeSecret;
 use std::net::TcpListener;
 
-use sqlx::PgPool;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use zero2prod::{
     configuration::get_configuration,
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
+
+fn configure_logger() {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -14,23 +19,14 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration");
 
-    let connection = PgPool::connect(
-        configuration
-            .database
-            .get_connection_string()
-            .expose_secret(),
-    )
-    .await
-    .expect("Failed to connect to Postgres.");
+    let connection = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(10))
+        .connect_lazy(&configuration.database.get_connection_string().expose_secret())
+        .expect("Unable to connect to Postgres.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     tracing::info!("Serving on http://{}", listener.local_addr().unwrap());
 
     run(listener, connection)?.await
-}
-
-fn configure_logger() {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
-    init_subscriber(subscriber);
 }
