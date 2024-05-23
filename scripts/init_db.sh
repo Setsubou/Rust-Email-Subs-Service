73f91898a633a1a6d5bf029860be77a03a2c7656
Setsubou <setsubou@protonmail.com>
2024-05-23 15:48:02 +0700

    Update sqlx to 0.7.4

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -7,7 +7,7 @@ set -eo pipefail
 if ! [ -x "$(command -v sqlx)" ]; then
     echo >&2 "Error: sqlx is not installed"
     echo >&2 "Use:"
-    echo >&2 "  cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
+    echo >&2 "  cargo install --version=0.7.4 sqlx-cli --no-default-features --features rsutls,postgres"
     echo >&2 "to install it."
     exit 1
 fi
